efaa0895c2bd85ed491643b14c8d61bccde8b3ab
Alan Chang <alannnchang@gmail.com>
2024-06-30 17:32:58 -0700

    basic event validation added

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -17,6 +17,39 @@ logging.basicConfig(
 )
 
 
+def random_event_generator():
+    '''
+    Event format example
+
+    event = {
+        event_type: feeling_ill,
+        priority: medium,
+        description: guest has stomach ache after eating 5 pieces of cake.
+    }
+    '''
+    event = random.choice(list(constants.event_list.items()))
+    event_type = random.choice(event[1])
+    priority = random.choice(list(constants.priority_list.keys()))
+    event = {
+        "event_type": event_type,
+        "priority": priority, 
+        "description": "what's the description for?"
+    }
+    return event
+
+
+def validate_type(event):
+    for key, values in constants.event_list.items():
+        if event["event_type"] in values:
+            return key
+    return None
+
+def validate_priority(event):
+    if event["priority"] in list(constants.priority_list.keys()):
+        return event["priority"]
+    return None
+
+
 def main():
     logging.info("Starting publisher")
     producer = KafkaProducer(
@@ -25,23 +58,25 @@ def main():
     )
     logging.info("Connected to Kafka broker.")
     logging.info(f"Will generate one unique event every {WAIT_TIME} seconds")
-
+    
     for i in range(1, EVENT_LIMIT):
-        event = random.choice(list(constants.event_list.items()))
-        event_category = event[0]
-        event_type = random.choice(event[1])
-        priority = random.choice(list(constants.priority_list.items()))
-        routine = random.choice(list(constants.routine_list.items()))
-        data = {
+        event = random_event_generator()
+        event_category = validate_type(event) 
+        priority = validate_priority(event)
+        if event_category == None or priority == None:
+            logging.info(f"Invalid event: {event}")
+            print(f"Invalid event: {event}")
+            continue
+        message = {
             "event_id": i,
             "event_category": event_category,
-            "event_type": event_type,
-            "priority": priority, 
-            "routine": routine
+            "event_type": event["event_type"],
+            "priority": event["priority"],
+            "description": event["description"]
         }
 
-        producer.send('test_topic', data)
-        logging.info(f"Sending event {i}...")
+        producer.send(f"event_category", message)
+        logging.info(f"Sending event {i}: {message}" )
         time.sleep(WAIT_TIME)
 
 if __name__ == "__main__":
