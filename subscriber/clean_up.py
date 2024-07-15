6158280878ff4a41900e339082f03f29babf05a5
Alan Chang <alannnchang@gmail.com>
2024-07-14 22:43:18 -0700

    enable_auto_commit=False for consumers

--- subscriber/clean_up.py
+++ subscriber/clean_up.py
@@ -15,8 +15,8 @@ consumer = KafkaConsumer(
         *kafka_topics,
         bootstrap_servers=['kafka-1:9092', 'kafka-2:9093', 'kafka-3:9094'],
         group_id='clean_up',
-        auto_offset_reset='earliest',
-        enable_auto_commit=True,
+        auto_offset_reset='earliest',  # Start reading at the earliest offset if no previous offset is found
+        enable_auto_commit=False,  # Disable auto-commit of offsets
         request_timeout_ms=20000,  # Timeout after 20 seconds
         retry_backoff_ms=500  # Backoff time between retries
     )
