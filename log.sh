7ec5475002ff4f51924b6dcf99c6d2d2cf421730
alannchang <alannnchang@gmail.com>
2024-09-13 14:27:19 -0700

    log.sh more concise now

--- log.sh
+++ log.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# ./show_log.sh container_name /path/to/log_file
+# ./show_log.sh container_namr
 
-# Check if two arguments are passed
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <container_name> <log_file>"
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <container_name>"
   exit 1
 fi
 
 CONTAINER_NAME=$1
-LOG_FILE=$2
+LOG_FILE="${CONTAINER_NAME}.log"
 
 docker exec -it "$CONTAINER_NAME" tail -f "$LOG_FILE"
